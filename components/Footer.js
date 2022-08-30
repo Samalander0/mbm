f832865331de01e0759de6d9221ed31b74da14f0
Sam Cheng <51756184+Samalander0@users.noreply.github.com>
2022-08-30 01:21:30 +0000

    Fixed an error

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,8 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <footer>
-      <h3>Beyond Blocks, by <a href="https://www.instagram.com/mustbeminecraft/" target="_blank">MustBeMinecraft</a></h3>
-      <p>Design & Development by <a href="https://www.samalander.dev" target="_blank">Sam Cheng</a></p>
+      <h3>Beyond Blocks, by <a href="https://www.instagram.com/mustbeminecraft/" target="_blank" rel="noreferrer">MustBeMinecraft</a></h3>
+      <p>Design & Development by <a href="https://www.samalander.dev" target="_blank" rel="noreferrer">Sam Cheng</a></p>
     </footer>
   )
 }
