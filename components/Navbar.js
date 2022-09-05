76dfcf9802a098ce94cbe89c6fcdb41990f8230a
Sam Cheng <51756184+Samalander0@users.noreply.github.com>
2022-09-05 02:28:30 +0000

    ACCOUNTS!

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,28 @@
 import Link from 'next/link'
+import { useContext } from 'react';
+import { UserContext } from '../lib/context';
 
 export default function Navbar() {
+  const { user, username } = useContext(UserContext);
+  const hasCourse = false;
+  
   return (
     <nav>
-      <div>
+      <a href="https://www.beyondblocks.club">
         <img src="/media/logo.svg"/>
-      </div>
+      </a>
       <div id="nav-links">
         <Link href="dashboard"><button className="btn brown" disabled>Dashboard</button></Link>
         <Link href="about"><button className="btn brown" disabled>About</button></Link>
         <Link href="preview"><button className="btn brown" disabled>Preview</button></Link>
       </div>
       <div id="nav-primary">
-        <Link href="join"><button className="btn green" disabled>Login</button></Link>
-        <Link href="join"><button className="btn-normal green" disabled>Get The Course</button></Link>
+        {!user && (
+          <Link href="join"><button className="btn green">Login</button></Link>
+        )}
+        {!hasCourse && (
+          <Link href="join"><button className="btn-normal green" disabled>Get The Course</button></Link>
+        )}
       </div>
     </nav>
   )
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,21 @@
 import '../styles/globals.scss'
 import Navbar from '../components/Navbar.js'
 import Footer from '../components/Footer.js'
+import { UserContext } from '../lib/context';
+import { useUserData } from '../lib/hooks';
 
 function MyApp({ Component, pageProps }) {
-  return (<>
-    <Navbar/>
-    <Component {...pageProps} />
-    <Footer/>
-    <script async src="https://zink.tips/zwc.js?rid=630bc5b9a7adfec34ded9ccf&c=gold"></script>
-  </>)
+  
+  const userData = useUserData();
+  
+  return (
+    <UserContext.Provider value={userData}>
+      <Navbar/>
+      <Component {...pageProps} />
+      <Footer/>
+      <script async src="https://zink.tips/zwc.js?rid=630bc5b9a7adfec34ded9ccf&c=gold"></script>
+    </UserContext.Provider>
+  )
 }
 
 export default MyApp
